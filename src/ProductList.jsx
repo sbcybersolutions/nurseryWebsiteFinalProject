324fb4220d7de1244578f2f92dbfa27400e67892
sbcybersolutions <christian@sbcyber.net>
2025-03-20 10:04:53 -0400

    added add to cart button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -284,6 +284,7 @@ function ProductList({ onHomeClick }) {
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">{plant.cost}</div>
+                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                                 </div>
                             ))}
                         </div>
