f173720404f1f940b99da7dfd0fc87d67aba8588
Christian S <christian@sbcyber.net>
2025-04-04 09:15:24 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
+import {BrowserRouter} from 'react"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <BrowserRouter basename="/nurseryWebsiteFinalProject"
+      <Provider store={store}>
+      <App />
+      </Provider>
+    </BrowserRouter> 
   </React.StrictMode>,
-)
+);
