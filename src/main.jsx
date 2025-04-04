3e4ec205e3631d66211c9b808b281b8105cb96df
Christian S <christian@sbcyber.net>
2025-04-04 09:18:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import store from './store.js'
-import {BrowserRouter} from 'react"
+import {BrowserRouter} from 'react'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/nurseryWebsiteFinalProject"
